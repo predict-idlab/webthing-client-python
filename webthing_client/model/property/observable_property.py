bba6ea07f66511eb8b2d5f0ddd0a276426e91b04
epadman <emile.deman@ugent.be>
2024-02-29 10:02:00 +0100

    Remove unused property properties

--- webthing_client/model/property/observable_property.py
+++ webthing_client/model/property/observable_property.py
@@ -11,30 +11,13 @@ class ObservableProperty:
 
     name: Optional[str] = None
 
-    realtime_datasource: Optional[str] = None
-
-    historical_datasource: Optional[str] = None
-
-    def __init__(self, iri: str, name: Optional[str], realtime_datasource: Optional[str], historical_datasource: Optional[str]) -> None:
+    def __init__(self, iri: str, name: Optional[str]) -> None:
         self.iri = iri
         self.name = name
-        self.realtime_datasource = realtime_datasource
-        self.historical_datasource = historical_datasource
     
     @classmethod
     def from_json_object(cls, json_object: Dict[str, Any]) -> ObservableProperty:
         return cls(
             json_object['$iri'],
-            json_object.get('label'),
-            json_object.get('realtimeDatasource'),
-            json_object.get('historicalDatasource')
+            json_object.get('label')
         )
-
-    def to_json_object(self) -> Dict[str, Any]:
-        return {
-            '$class': self.type,
-            '$iri': self.iri,
-            'label': self.name,
-            'realtimeDatasource': self.realtime_datasource,
-            'historicalDatasource': self.historical_datasource
-        }
