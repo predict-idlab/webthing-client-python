6dde62b1921ea7bb6e6384cfc63eaddb96b9b7ab
epadman <emile.deman@ugent.be>
2024-03-20 13:10:04 +0100

    Improve stomp websocket handling

--- webthing_client/stomp.py
+++ webthing_client/stomp.py
@@ -9,12 +9,12 @@ import stomper
 class StompWebsocket:
     """A class for setting up and managing a Stomp websocket."""
     
-    def __init__(self, ws_uri: str, reconnect_timeout_ms: int=1_000, verbose: bool=False) -> None:
+    def __init__(self, ws_uri: str, reconnect_timeout_ms: int=60_000, verbose: bool=False) -> None:
         """A class for setting up and managing a Stomp websocket.
 
         Args:
             ws_uri (str): The websocket URI to connect to, should be of protocol type 'ws://' or 'wss://' i.g. 'ws://webthing.example.com'.
-            reconnect_timeout_ms (int, optional): Reconnect timeout. Defaults to 1_000.
+            reconnect_timeout_ms (int, optional): Reconnect timeout. Defaults to 60_000.
             verbose (bool, optional): Verbose logging. Defaults to False.
         """
         self._ws_uri = ws_uri
@@ -25,6 +25,10 @@ class StompWebsocket:
         self._backlog_subscriptions: Dict[str, List[Callable[[str], None]]] = {}
         self._subscriptions: Dict[str, List[Callable[[str], None]]] = {}
         self._next_sub_id  = 0
+
+        self._reconnect = False
+        self._reconnect_status_code = None
+        self._reconnect_message = None
 
         self._init_websocket()
 
@@ -40,13 +44,26 @@ class StompWebsocket:
 
     def _on_open(self, ws: WebSocketApp):
         # Send stomp connect message on websocket open
-        self._ws.send("CONNECT\naccept-version:1.0,1.1,2.0\n\n\x00\n")
+        # We don't want to send heartbeats but want to receive heartbeats
+        self._ws.send("CONNECT\naccept-version:1.1\nheart-beat:0,25000\n\n\x00\n")
 
     def _on_message(self, ws: WebSocketApp, stomp_message: str) -> None:
+        # If an empty message it is a heartbeat
+        if stomp_message.strip(" \n") == "":
+            if self._verbose:
+                print("Received heartbeat")
+            return
         # Process stomp message from websocket
         message = stomper.Frame.unpack(stomper.Frame(), stomp_message)
         if message['cmd'] == 'CONNECTED':
             self._connected_message()
+            if self._reconnect:
+                print(f"WebsocketClient <{self._ws_uri}> reconnected")
+            # Reset reconnect
+            self._reconnect = False
+            self._reconnect_error = None
+            self._reconnect_status_code = None
+            self._reconnect_message = None
             return
         elif message['cmd'] == 'MESSAGE':
             destination = message['headers']['destination']
@@ -65,8 +82,11 @@ class StompWebsocket:
         self._backlog_subscriptions = {}
 
     def _on_error(self, ws: WebSocketApp, error: WebSocketException) -> None:
-        print(f"Error on WebsocketClient <{self._ws_uri}>:")
-        print(error)
+        if not self._reconnect:
+            # If first error store
+            self._reconnect_error = error
+        else:
+            print(f"WebsocketClient <{self._ws_uri}> error: {error}")
 
     def _on_close(self, ws: WebSocketApp, status_code: int, message: str) -> None:
         # Set connected to false and set backlog
@@ -75,10 +95,26 @@ class StompWebsocket:
         self._subscriptions = {}
         self._next_sub_id = 0
 
-        print(f"WebsocketClient <{self._ws_uri}> closed with status {status_code} and message: {message}")
-        print(f"Retrying connection on <{self._ws_uri}> in {self._reconnect_timeout_ms} ms...")
-        time.sleep(self._reconnect_timeout_ms/1000)
-        self._init_websocket()
+        # If first disconnect try to connect immediately, it may just be connection cleanup
+        if not self._reconnect:
+            self._reconnect = True
+            self._reconnect_status_code = status_code
+            self._reconnect_message = message
+            self._init_websocket()
+        else:
+            print(f"WebsocketClient <{self._ws_uri}> closed with status {status_code} and message: {message}, retrying in {self._reconnect_timeout_ms/1000} seconds...")
+            # Print first error
+            if self._reconnect_error is not None:
+                print(f"WebsocketClient <{self._ws_uri}> first error: {self._reconnect_error}")
+                self._reconnect_error = None
+            # Print first disconnect
+            if self._reconnect_status_code is not None or self._reconnect_message is not None:
+                print(f"WebsocketClient <{self._ws_uri}> first closed with status {self._reconnect_status_code} and message: {self._reconnect_message}")
+                self._reconnect_status_code = status_code = None
+                self._reconnect_message = message = None
+            # Retry after timeout
+            time.sleep(self._reconnect_timeout_ms/1000)
+            self._init_websocket()
 
 
     def subscribe(self, destination: str, callback: Callable[[str], None]) -> None:
